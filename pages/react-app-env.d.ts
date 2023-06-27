01b2bff35e061046cc2839e21091d7c8304b32b3
zach <97859794+zvchvry@users.noreply.github.com>
2023-06-27 16:02:37 -0400

    Update react-app-env.d.ts

--- pages/react-app-env.d.ts
+++ pages/react-app-env.d.ts
@@ -1,5 +1,9 @@
-/// <reference types="react-scripts" />
 
-interface Window {
-    ethereum: any
+
+import { MetaMaskInpageProvider } from "@metamask/providers";
+
+declare global {
+  interface Window{
+    ethereum?:MetaMaskInpageProvider
+  }
 }
