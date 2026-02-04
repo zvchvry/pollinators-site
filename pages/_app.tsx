6fda5557bf93991f2c2ba84139398b907de7b4b8
zach <97859794+zvchvry@users.noreply.github.com>
2026-02-04 00:07:24 -0500

    another 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,61 +1,50 @@
 import '../styles/style.css'
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
-import type { AppProps } from 'next/app';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { mainnet } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
-import Head from 'next/head';
-import
- { Analytics } 
-from
- 
-'@vercel/analytics/react'
-;
+import '@rainbow-me/rainbowkit/styles.css'
 
-const { chains, provider, webSocketProvider } = configureChains(
-  [
-    mainnet,
-  ],
-  [publicProvider()]
-);
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-const { connectors } = getDefaultWallets({
+import { ConnectButton, RainbowKitProvider, darkTheme, getDefaultConfig } from '@rainbow-me/rainbowkit'
+import { WagmiProvider } from 'wagmi'
+import { mainnet } from 'wagmi/chains'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Analytics } from '@vercel/analytics/react'
+
+const config = getDefaultConfig({
   appName: 'RainbowKit App',
-  chains,
-});
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || 'YOUR_PROJECT_ID',
+  chains: [mainnet],
+  ssr: true,
+})
 
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-  webSocketProvider,
-});
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains} theme={darkTheme()}>
-        <Head>
-        <meta charSet="UTF-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1" />
-  <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-   <link rel="icon" type="image/png" href="/favicon.ico" />
-  <title>POLLINATORS</title>
-  <meta name="description" content="Pollinators NFT & Flower Banners" />
-        </Head>
-      <center>
-    <div id="header">
-    <ConnectButton />
-     
-    </div>
-  </center>
-        <Component {...pageProps} />
-        <Analytics />
-      </RainbowKitProvider>
-    </WagmiConfig>
-  );
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider theme={darkTheme()}>
+          <Head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width,initial-scale=1" />
+            <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+            <link rel="icon" type="image/png" href="/favicon.ico" />
+            <title>POLLINATORS</title>
+            <meta name="description" content="Pollinators NFT & Flower Banners" />
+          </Head>
+
+          <center>
+            <div id="header">
+              <ConnectButton />
+            </div>
+          </center>
+
+          <Component {...pageProps} />
+          <Analytics />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
 }
 
-export default MyApp;
+export default MyApp
